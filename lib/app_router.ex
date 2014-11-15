21215471a260de98a97c391f733fc200f3fe4923
Rodrigo Oliveira <rodrigo.dealer@gmail.com>
2014-11-15 12:08:02 -0200

    Adding router

---
+++ lib/app_router.ex
@@ -0,0 +1,19 @@
+defmodule AppRouter do
+  import Plug.Conn
+  use Plug.Router
+
+  plug :match
+  plug :dispatch
+
+  get "/" do
+    send_resp(conn, 200, "Hello world")
+  end
+
+  get "/hello" do
+    send_resp(conn, 200, "world")
+  end
+
+  match _ do
+    send_resp(conn, 404, "OPS!")
+  end
+end
--- lib/rest_api.ex
+++ lib/rest_api.ex
@@ -1,2 +1,13 @@
 defmodule RestApi do
+
+  def init(options) do
+    options
+  end
+
+  def call(conn, _opts) do
+    AppRouter.call(conn, AppRouter.init([]))
+  end
 end
+
+IO.puts "Running RestApi with Cowboy on http://localhost:4000"
+Plug.Adapters.Cowboy.http RestApi, []
